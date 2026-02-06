e25ea4ffb77df8c3db98e6c809ef3de488368080
Subhra Kinkar <96880067+Suvra03@users.noreply.github.com>
2026-02-06 13:32:33 +0530

    Delete auth-service directory

--- auth-service/src/main/java/com/insurance/auth_service/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.insurance.auth_service.exception;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
